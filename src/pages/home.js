e809a05f4d76c67d9663fd30ae3ab1d279e74a07
Pranav Rana <ranapranav00@gmail.com>
2024-09-04 23:51:08 -0400

    Updated Resume Link

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,7 +25,7 @@ function Home() {
                     <Button id="button" href="https://github.com/ranapranav00"><GitHub id="gh-icon" className="icon"/>GitHub</Button>
                     <Button id="button" href="https://www.instagram.com/pranav_0629/"><Instagram id="ig-icon" className="icon"/>Instagram</Button>
                     <Button id="button" href="mailto:ranapranav00@gmail.com"><Email id="gm-icon" className="icon"/>Email</Button>
-                    <Button id="button" href="https://drive.google.com/file/d/13JInAS9OOD9Od_58oXhQuF3OlYc33SWf/view?usp=sharing"><AssignmentInd id="r-icon" className="icon"/>Resume</Button>
+                    <Button id="button" href="https://drive.google.com/file/d/1rFQZil367mhVwVrWU3_YlBYrKTVdbik3/view?usp=sharing"><AssignmentInd id="r-icon" className="icon"/>Resume</Button>
                 </div>
             </div>
         </div>
