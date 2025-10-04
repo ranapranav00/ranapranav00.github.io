a290c95d01f30c8309967896878f49b518c957a3
Pranav Rana <ranapranav00@gmail.com>
2025-10-04 15:21:18 -0400

    Setup for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { componentTagger } from "lovable-tagger"
 
 export default defineConfig(({ mode }) => ({
+  base: '/', // Root deployment
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
