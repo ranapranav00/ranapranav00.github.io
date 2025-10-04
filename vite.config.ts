b8b10ea64a776d2c2b4e2ecb192ef4f316d30a7a
Pranav Rana <ranapranav00@gmail.com>
2025-10-04 15:11:21 -0400

    Update config files

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { componentTagger } from "lovable-tagger"
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: "::",
+    port: 8080
+  }
+}))
